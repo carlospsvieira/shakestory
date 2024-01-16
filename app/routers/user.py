5de8d814ed3a0967f8694ba5e245077024375a6b
carlos <carlepsvieira@gmail.com>
2024-01-16 11:31:42 -0300

    add delete user function

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Path
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from app.db.session import get_db
@@ -29,3 +29,18 @@ async def register_user(user: UserCreate, db: Session = Depends(get_db)):
             "user": user_dict,
         },
     )
+
+
+@router.delete("/users/{user_id}", response_model=dict)
+async def delete_user(
+    user_id: int = Path(..., title="The ID of the user to delete", ge=1),
+    db: Session = Depends(get_db),
+):
+    user = user_service.get_user_by_id(db, user_id)
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user_service.delete_user(db, user_id)
+
+    return {"message": "User deleted successfully"}
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,16 +1,29 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app.db.base import User
 from app.models.requests import UserCreate
+import bcrypt
 
 
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
 
 
+def get_user_by_id(db: Session, user_id: int):
+    user = db.query(User).filter(User.id == user_id).first()
+    return user
+
+
 def create_user(db: Session, user: UserCreate):
-    hashed_password = user.password
+    hashed_password = bcrypt.hashpw(user.password.encode("utf-8"), bcrypt.gensalt())
     db_user = User(username=user.username, email=user.email, password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def delete_user(db: Session, user_id: int):
+    user = db.query(User).filter(User.id == user_id).first()
+    db.delete(user)
+    db.commit()
