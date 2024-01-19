a9e8958a2aeb07447b47c32b3ef5cb0ad22f8b18
carlos <carlepsvieira@gmail.com>
2024-01-19 18:06:20 -0300

    start create story

--- app/routers/story.py
+++ app/routers/story.py
@@ -3,6 +3,31 @@ from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.services import story_service
-from app.models.requests import StoryCreate
+from app.models.requests import StoryCreate, StoryResponse
 
 router = APIRouter()
+
+
+@router.post("/story/new", response_model=StoryCreate)
+async def new_story(story: StoryCreate, db: Session = Depends(get_db)):
+    existing_story = story_service.get_story_by_title(db, story.title)
+    if existing_story:
+        raise HTTPException(status_code=400, detail="Title already registered")
+
+    created_story = story_service.create_story(db, story)
+
+    story_response = StoryResponse(
+        id=created_story.id,
+        title=created_story.title,
+        genre=created_story.genre,
+        current_segment_id=created_story.current_segment_id,
+        creation_date=created_story.creation_date,
+    ).model_dump()
+
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content={
+            "message": "Story created successfully",
+            "story": story_response,
+        },
+    )
---
+++ app/services/story_service.py
@@ -0,0 +1,16 @@
+from app.db.base import Story
+from app.models.requests import StoryCreate
+from sqlalchemy.orm import Session
+
+
+def get_story_by_title(db: Session, title: str):
+    story = db.query(Story).filter(Story.title == title).first()
+    return story
+
+
+def create_story(db: Session, story: StoryCreate):
+    db_story = Story(title=StoryCreate.title, genre=StoryCreate.genre)
+    db.add(db_story)
+    db.commit()
+    db.refresh(db_story)
+    return db_story
